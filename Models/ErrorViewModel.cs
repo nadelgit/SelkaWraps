e025ee368b3118427a40e7f541ee44593934c34b
Nada Adel <nada_adel_mostafa@hotmail.com>
2025-08-15 13:51:25 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SelkaWraps.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
